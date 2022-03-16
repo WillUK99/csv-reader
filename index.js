ef5dce67e0b5ee377af4a47e3d3bd0df517f2225
WillUK99 <willuk1999@gmail.com>
2022-03-16 17:48:58 +0000

    Streaming in csv data and logging data

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("testing testing 123")
+const parse = require("csv-parser")
+const fs = require("fs")
+const log = console.log
+
+const results = []
+
+fs.createReadStream("DATA.csv")
+    .pipe(parse({
+        columns: true,
+    }))
+    .on("data", (row) => results.push(row))
+    .on("error", (err) => log(err))
+    .on("end", () => console.log(results))
+
