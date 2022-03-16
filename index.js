8889d703a8d7baa47536017ce86fca919fe1e9eb
WillUK99 <willuk1999@gmail.com>
2022-03-16 20:51:49 +0000

    Duplicate sku's now skipped

--- index.js
+++ index.js
@@ -10,50 +10,44 @@ const log = console.log
  * - If results[n] has matching SKU id to existing object -> store in skipped arr
  */
 
+// Caches and storage
 const results = []
-
-const createdCache = {}
-const unchangedCache = {}
 const skipped = []
+
+const created = {}
+const unchanged = {}
 
 function hasBlankFields(data) {
     return Object.values(data).some((field) => field === "" || field === undefined);
 }
 
+function generateCreatedData(data) {
+    if (!created[data.SKU]) {
+        created[data.SKU] = data
+    } else {
+        log(data, "is a duplicate and has been skipped")
+        skipped.push(data)
+    }
+}
+
+// Reading stream and piping through csv-parser
 fs.createReadStream("DATA.csv")
     .pipe(parse({
         columns: true,
     }))
     .on("data", (data) => {
         results.push(data)
-        hasBlankFields(data)
+
         if (hasBlankFields(data)) {
             skipped.push(data)
             log(data, "has been skipped due to blank fields")
         }
+
+        generateCreatedData(data)
     })
     .on("error", (err) => log(err))
     .on("end", () => {
-        log(`Number of products created`)
-        log(`Number of products unchanged`)
+        log(`Number of products created: ${Object.entries(created).length}`)
+        log(`Number of products unchanged:`)
         log(`Number of products skipped: ${skipped.length}`)
     })
-
-
-
-
-
-// O(n^2) will make more efficient 
-// function parseData() {
-    // for (let row of results) {
-        // for (let key in row) {
-            // if (!row[key]) {
-                // log( row, "has been skipped due to blank values")
-                // skipped.push(row)
-            // }
-        // }
-    // }
-    // log(`Number of rows skipped: ${skipped.length}`)
-// }
-
-
