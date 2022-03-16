1ba7944f9eb02944b3024c9ee5a0b478be6e74ea
WillUK99 <willuk1999@gmail.com>
2022-03-16 21:18:12 +0000

    no.of created output fixed

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const log = console.log
 
 /**
  * TODO
- * - Loop through results arr 
- * - If results[n] has any blank fields skip -> store in skipped arr
- * - Elements that don't have blank fields add to seperate object -> store in createdCache
- * - If results[n] has matching SKU id to existing object -> store in skipped arr
+ * - Loop through results arr - done
+ * - If results[n] has any blank fields skip -> store in skipped arr - done
+ * - Elements that don't have blank fields add to seperate object -> store in created - done
+ * - If results[n] has matching SKU id to existing object -> store in skipped arr - done
  */
 
 // Caches and storage
@@ -15,7 +15,6 @@ const results = []
 const skipped = []
 
 const created = {}
-const unchanged = {}
 
 function hasBlankFields(data) {
     return Object.values(data).some((field) => field === "" || field === undefined);
@@ -24,8 +23,7 @@ function hasBlankFields(data) {
 function generateCreatedData(data) {
     if (!created[data.SKU]) {
         created[data.SKU] = data
-    } else {
-        log(data, "is a duplicate and has been skipped")
+    } else if (created[data.SKU]) {
         skipped.push(data)
     }
 }
@@ -36,18 +34,19 @@ fs.createReadStream("DATA.csv")
         columns: true,
     }))
     .on("data", (data) => {
-        results.push(data)
+        results.push(data) // pushing to results arr incase needed somewhere
 
-        if (hasBlankFields(data)) {
+        if (hasBlankFields(data)) { 
             skipped.push(data)
-            log(data, "has been skipped due to blank fields")
+            log(data, "contains blank data fields, this data has been skipped")
+        } else {
+            generateCreatedData(data) // will build up new array from non duped sku id's
         }
 
-        generateCreatedData(data)
     })
     .on("error", (err) => log(err))
     .on("end", () => {
         log(`Number of products created: ${Object.entries(created).length}`)
-        log(`Number of products unchanged:`)
+        log(`Number of products unchanged: ${Object.entries(created).length - skipped.length}`)
         log(`Number of products skipped: ${skipped.length}`)
     })
