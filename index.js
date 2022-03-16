ef32fed5cd9ee3f6c23674b647d5a889d7a8f759
WillUK99 <willuk1999@gmail.com>
2022-03-16 18:07:52 +0000

    Adding CSV row's with blank fields to skippedCache

--- index.js
+++ index.js
@@ -10,5 +10,31 @@ fs.createReadStream("DATA.csv")
     }))
     .on("data", (row) => results.push(row))
     .on("error", (err) => log(err))
-    .on("end", () => console.log(results))
+    .on("end", parseData)
+
+
+/**
+ * TODO
+ * - Loop through results arr 
+ * - If results[n] has any blank fields skip -> store in skippedCache
+ * - Elements that don't have blank fields add to seperate object -> store in createdCache
+ * - If results[n] has matching SKU id to existing object -> store in skippedCache
+ */
+
+const createdCache = {}
+const unchangedCache = {}
+const skippedCache = {}
+
+// O(n^2) will make more efficient 
+function parseData() {
+    for (let row of results) {
+        for (let key in row) {
+            if (!row[key]) {
+                skippedCache[key] = row
+            }
+        }
+    }
+    log(skippedCache)
+}
+
 
