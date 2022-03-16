3de202915fdd6f01ad807a42358f23b4dccef169
WillUK99 <willuk1999@gmail.com>
2022-03-16 21:44:08 +0000

    Create new CSV with data and writing to file.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const log = console.log
 // Caches and storage
 const results = []
 const skipped = []
-
 const created = {}
 
 function hasBlankFields(data) {
@@ -36,17 +35,48 @@ fs.createReadStream("DATA.csv")
     .on("data", (data) => {
         results.push(data) // pushing to results arr incase needed somewhere
 
-        if (hasBlankFields(data)) { 
+        if (hasBlankFields(data)) {
             skipped.push(data)
             log(data, "contains blank data fields, this data has been skipped")
         } else {
             generateCreatedData(data) // will build up new array from non duped sku id's
         }
-
     })
     .on("error", (err) => log(err))
     .on("end", () => {
+        log("Number of initial product rows:", results.length)
         log(`Number of products created: ${Object.entries(created).length}`)
-        log(`Number of products unchanged: ${Object.entries(created).length - skipped.length}`)
+        log(`Number of products unchanged: ${Object.entries(created).length - skipped.length}`) // unchanged meaning how many rows of data were not altered?
         log(`Number of products skipped: ${skipped.length}`)
+
+        /**
+         * Figure out how to reread the CSV on subsequent runs of this script.
+         * - generate CSV string from created array?
+         * - write to new CSV string to DATA.csv to be reread at a later date?
+         */
+
+        // generate CSV string from array of objects
+        const csvString = [
+            [
+                "SKU",
+                "Colour",
+                "Size",
+            ],
+
+            ...Object.values(created).map((item) => [
+                item.SKU,
+                item.Colour,
+                item.Size,
+            ])
+        ]
+        .map((element) => element
+        .join(",")).join("\n")
+
+        // Overriding old CSV with new CSV data
+        fs.writeFile("DATA.csv", csvString, err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
     })
